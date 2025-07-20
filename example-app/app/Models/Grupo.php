6055c536409c3eee8be18b217a35386dd3fb847c
Aleczsis <alexis1777.najera@gmail.com>
2025-07-20 13:46:17 -0600

    AGregue la vista de hrupos manual

---
+++ example-app/app/Models/Grupo.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Grupo extends Model
+{
+    protected $table = 'grupos';
+    protected $primaryKey = 'id_grupo';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'clave',
+        'carrera',
+        'created_by',
+        'modified_by'
+    ];
+}
